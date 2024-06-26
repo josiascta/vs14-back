2e3e0fd740b5672a064f040568f942835a37b47b
Lucas Seixas <lucasseixas29@hotmail.com>
2024-06-26 20:05:19 -0300

    Criando a classe Gerenciador Banco

--- 01-Java/Task-03/GerenciadorBanco.java
+++ 01-Java/Task-03/GerenciadorBanco.java
@@ -1,2 +1,47 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class GerenciadorBanco {
+
+    private List<ContaBancaria> contas = new ArrayList<ContaBancaria>();
+
+    public GerenciadorBanco(List<ContaBancaria> contas) {
+        this.contas = contas;
+    }
+
+    public void adicionarConta(ContaBancaria conta) {
+        contas.add(conta);
+    }
+
+    public void removerConta(String numeroConta) {
+        boolean isFound = false;
+        for (int i = 0; i < contas.size(); i++) {
+            if (contas.get(i).getNumeroConta().equals(numeroConta)) {
+                contas.remove(i);
+                isFound = true;
+                break;
+            }
+        }
+        if (!isFound) {
+            System.out.println("Conta não encontrada.");
+        }
+    }
+
+    public ContaBancaria buscarConta(String numeroConta) {
+        for (int i = 0; i < contas.size(); i++) {
+            if (contas.get(i).getNumeroConta().equals(numeroConta)) {
+                return contas.get(i);
+            }
+        }
+        return null;
+    }
+
+    public void listarContas() {
+        for (ContaBancaria conta : contas) {
+            System.out.println("Número conta: " + conta.getNumeroConta());
+            System.out.println("Titular conta: " + conta.getTitular());
+            System.out.println("Saldo: " + conta.getSaldo());
+            System.out.println();
+        }
+    }
 }
