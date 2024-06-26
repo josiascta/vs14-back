8da9d52bd73b42f2799b3227d0ca75f47a81c402
Josias <josiasjt3@gmail.com>
2024-06-26 19:31:06 -0300

    Adicionando classe ContaBancaria.

---
+++ 01-Java/Task-03/ContaBancaria.java
@@ -0,0 +1,48 @@
+public class ContaBancaria {
+
+    private String numeroConta;
+    private String titular;
+    private double saldo;
+
+    public ContaBancaria(){}
+
+    public ContaBancaria(String numeroConta, String titular, double saldo) {
+        this.numeroConta = numeroConta;
+        this.titular = titular;
+        this.saldo = saldo;
+    }
+
+    public String getNumeroConta() {
+        return numeroConta;
+    }
+
+    public void setNumeroConta(String numeroConta) {
+        this.numeroConta = numeroConta;
+    }
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public void setTitular(String titular) {
+        this.titular = titular;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public void depositar(double valor){
+        this.saldo += valor;
+    }
+
+    public void sacar(double valor){
+        if(this.saldo >= valor){
+            this.saldo -= valor;
+        }
+    }
+}
